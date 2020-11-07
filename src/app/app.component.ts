b794256777d069f0ef71d3f0c83576874519718e
Alexis <omaroquendo221@gmail.com>
2020-11-07 17:15:49 -0500

    Diseño inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Product} from './product.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,44 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Tienda';
+  items = ['Nicolas', 'Alexis', 'El perez'];
+
+  products: Product[] = [
+    {
+      id: '1',
+      image: 'assets/images/imagen1.jpg',
+      title: 'Chaqueta',
+      price: 80000,
+      description: ''
+    },
+    {
+      id: '2',
+      image: 'assets/images/imagen2.jpg',
+      title: 'Conjunto',
+      price: 80000,
+      description: ''
+    },
+    {
+      id: '3',
+      image: 'assets/images/imagen3.jpg',
+      title: 'Conjunto',
+      price: 80000,
+      description: ''
+    },
+    {
+      id: '4',
+      image: 'assets/images/imagen4.jpg',
+      title: 'Camisa blanca',
+      price: 80000,
+      description: ''
+    },
+  ];
+
+  addItems(){
+    this.items.push('nuevo item');
+  }
+
+  deleteItems(index: number){
+    this.items.splice(index, 1);
+  }
 }
