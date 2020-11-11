93647efc10a2c12a09c1a9c5afbbadcfcf124ef0
Alexis <omaroquendo221@gmail.com>
2020-11-10 20:26:20 -0500

    Servicio para obtener todos los productos y producto por id

---
+++ src/app/products.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import {Product} from './product.model';
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+
+  products: Product[] = [
+    {
+      id: '1',
+      image: 'assets/images/imagen1.jpg',
+      title: 'Chaqueta',
+      price:  80000,
+      description: ''
+    },
+    {
+      id: '2',
+      image: 'assets/images/imagen2.jpg',
+      title: 'Conjunto',
+      price: 80000,
+      description: ''
+    },
+    {
+      id: '3',
+      image: 'assets/images/imagen3.jpg',
+      title: 'Conjunto',
+      price: 80000,
+      description: ''
+    },
+    {
+      id: '4',
+      image: 'assets/images/imagen4.jpg',
+      title: 'Camisa blanca',
+      price: 80000,
+      description: ''
+    },
+  ];
+
+  constructor() {}
+
+    getAllProducts(){
+      return this.products;
+    }
+    getProduct(id: string){
+      return this.products.find(item =>id === item.id);
+    } 
+}
